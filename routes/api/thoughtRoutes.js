532fe15d4259e56f7899ab5e65374002b736edc9
Andrea Hergert <ahergert24@hotmail.com>
2022-12-16 13:56:35 -0800

    added user and thoughts routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+
+const {
+    getThought,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction
+} = require('../../controllers/thoughtController');
+
+// /api/thoughts
+router.route('/').get(getThought).post(createThought);
+
+// /api/thoughts/:thoughtId
+router.route('/:thoughtId')
+.get(getSingleThought)
+.put(updateThought)
+.delete(deleteThought);
+
+//  /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions')
+.post(createReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route('/:thoughtId/reactions/:reactionId')
+.delete(deleteReaction);
+
+
+module.exports = router;
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,27 @@
+const router = require('express').Router();
+
+const {
+    getUser,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/').get(getUser).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId')
+.get(getSingleUser)
+.put(updateUser)
+.delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId')
+.post(addFriend)
+.delete(deleteFriend);
+
+module.exports = router;
