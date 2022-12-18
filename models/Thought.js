ec6b9bf4ecd51a0c62b171c4bcd19d631e00e319
Andrea Hergert <ahergert24@hotmail.com>
2022-12-17 19:06:35 -0800

    switched schema order

--- models/Thought.js
+++ models/Thought.js
@@ -2,6 +2,36 @@ const { Schema, model, Types } = require('mongoose');
 
 const moment = require('moment')
 
+const reactionSchema = new Schema (
+  {
+     reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+     },
+     reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280
+     },
+     username: {
+      type: String,
+      required: true,
+     },
+     createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
+     },
+  },
+  {
+      toJSON: {
+          virtuals: true,
+          getters: true
+      },
+      id: false,
+  }
+)
+
 const thoughtSchema = new Schema (
     {
       thoughtText: {
@@ -25,36 +55,6 @@ const thoughtSchema = new Schema (
         toJSON: {
             virtuals: true,
             getters: true,
-        },
-        id: false,
-    }
-)
-
-const reactionSchema = new Schema (
-    {
-       reactionId: {
-        type: Schema.Types.ObjectId,
-        default: () => new Types.ObjectId(),
-       },
-       reactionBody: {
-        type: String,
-        required: true,
-        maxlength: 280
-       },
-       username: {
-        type: String,
-        required: true,
-       },
-       createdAt: {
-        type: Date,
-        default: Date.now,
-        get: createdAtVal => moment(createdAtVal).format("MMM DD, YYYY [at] hh:mm a"),
-       },
-    },
-    {
-        toJSON: {
-            virtuals: true,
-            getters: true
         },
         id: false,
     }
